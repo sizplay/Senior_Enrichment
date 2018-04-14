3c9fa3917c376af9c8ba612d94fad8b01ee6f3b6
Chaehoon Lim <sizplay@gmail.com>
2018-04-13 22:17:31 -0400

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,8 @@ const Home = () => {
           <p>
           I have some issues. <br /> 1. Please refresh /students page after deleting a Campus. <br />
           2. Please click text in some buttons instead of edge of the buttons.<br />
-          3. When you validate student GPA, it is only validated for empty. <br /> it should be validated for not number and min 0 to max 4. <br /> Please use seed file when you need data.
+          3. When you validate student GPA, it is only validated for empty. <br /> it should be validated for not number and min 0 to max 4. <br /> Please use seed file when you need data.<br /><br />
+            https://www.youtube.com/watch?v=3FtRxT2S5R0
           </p>
         </div>
       </div>
